a8b057e1d38f29f0799153643f9a085279805e41
Colin Quelle <cquelle3@binghamton.edu>
2021-03-17 22:04:42 -0400

    Added xcode project

---
+++ SoundApp/SoundAppTests/SoundAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  SoundAppTests.swift
+//  SoundAppTests
+//
+//  Created by Colin Quelle on 3/17/21.
+//  Copyright © 2021 Colin Quelle. All rights reserved.
+//
+
+import XCTest
+@testable import SoundApp
+
+class SoundAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
